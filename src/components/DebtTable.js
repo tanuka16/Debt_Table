3036300e7c6e80682b9210ce874bd2a56beab3c4
Tanuka16 <tanuka.das12@gmail.com>
2020-04-17 23:12:22 -0400

    rendered the data

--- src/components/DebtTable.js
+++ src/components/DebtTable.js
@@ -2,9 +2,38 @@ import React, { Component } from 'react';
 
 
 class DebtTable extends Component {
+
+
   render(){
     return (
       <div>
+        <table>
+        <thead>
+        <tr>
+          <th>CreditorName</th>
+          <th>FirstName</th>
+          <th>LastName</th>
+          <th>MinPaymentPercentage</th>
+          <th>Balance</th>
+        </tr>
+        </thead>
+          <tbody>
+          {
+            this.props.debt.map(debt => {
+              return(
+                  <tr key={debt.id}>
+                      <td>{debt.creditorName}</td>
+                      <td>{debt.firstName}</td>
+                      <td>{debt.lastName}</td>
+                      <td>{debt.minPaymentPercentage}</td>
+                      <td>{debt.balance}</td>
+                    </tr>)
+
+                })
+              }
+          </tbody>
+        </table>
+
 
       </div>
     );

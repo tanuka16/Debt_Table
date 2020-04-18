cb65df92ad8ea04f5286408bccdf05f1cb93c67f
Tanuka16 <tanuka.das12@gmail.com>
2020-04-18 00:16:10 -0400

    added checkboxes, select each event and css

--- src/components/DebtTable.js
+++ src/components/DebtTable.js
@@ -4,12 +4,24 @@ import React, { Component } from 'react';
 class DebtTable extends Component {
 
 
+  //Select each checkbox === done
+  onSelectChange =(e)=>{
+    // e.preventDefault()
+    this.setState({ [e.target.name]: e.target.checked })
+    console.log(e.target.checked);
+  }
+
+
   render(){
     return (
       <div>
         <table>
         <thead>
         <tr>
+          <th className="select_all">
+            <input type="checkbox" 
+            />
+          </th>
           <th>CreditorName</th>
           <th>FirstName</th>
           <th>LastName</th>
@@ -22,13 +34,17 @@ class DebtTable extends Component {
             this.props.debt.map(debt => {
               return(
                   <tr key={debt.id}>
+                      <td className="select">
+                        <input type="checkbox" name="check1" id="child_check"
+                          onChange={this.onSelectChange.bind(this)}
+                         />
+                      </td>
                       <td>{debt.creditorName}</td>
                       <td>{debt.firstName}</td>
                       <td>{debt.lastName}</td>
                       <td>{debt.minPaymentPercentage}</td>
                       <td>{debt.balance}</td>
                     </tr>)
-
                 })
               }
           </tbody>
